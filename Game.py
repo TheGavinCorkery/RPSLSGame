bd9e88eb411d749d8ff46024a04c63a630ed0022
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-09-10 16:32:52 -0400

    tested player selection and naming
    tested player selection and naming

--- Game.py
+++ Game.py
@@ -3,30 +3,36 @@ from Human import Human
 
 class Game:
     def __init__(self):
-        pass
+        self.player_one = ''
+        self.player_two = ''
+        self.score_to_win = 2
     def run_game(self):
         pass
-
     def welcome_message(self):
         #### Display rules and welcome
         pass
     def player_selection(self):
+        player_one = Human()
         user_input = 'playerz'
         while user_input == 'playerz':
             user_input = input("Would you like to play single-player or multiplayer? Enter '1' for single-player or '2' for multiplayer.")
             if user_input == '1':
                 print("You have selected single-player! You will play against an AI opponent.")
-                player_one = input("What is your name?")
-                player_two = "Player2"
+                player_one.set_name = input("What is your name?")
+                player_two = AI()
+
             elif user_input == '2':
+                player_two = Human()
                 print("You have selected multiplayer! please enter your name below. ")
-                player_one = input("Player 1, what is your real name??")
-                player_two = input("What is player 2's name?")
+                player_one.set_name = input("Player 1, what is your real name??")
+                player_two.set_name = input("What is player 2's name?")
             else:
                 user_input = 'playerz'
                 print("Let's make a logical selection here")
         self.player_one = player_one
         self.player_two = player_two
+        print(self.player_one)
+        print(self.player_two)
     def compare_player_choices(self, player1, player2):
         # Rock crushes Scissors
         # Rock crushes Lizard
@@ -45,16 +51,7 @@ class Game:
         # 4 — scissors 
         #each choice wins against the preceding two choices and loses against the following two choices 
         # (if rock and scissors are thought of as being adjacent using modular arithmetic).
-        if player1 == "rock":
-            player1num = int(0)
-        if player1 == "Spock":
-            player1num = int(1)
-        if player1 == "paper":
-            player1num = int(2)
-        if player1 == "lizard":
-            player1num = int(3)
-        if player1 == "scissors":
-            player1num = int(4)
+        pass
         
 
     def best_of_wins(self):
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Player import Player
-
+from Game import Game
 player1 = Player()
 
 player1.display_gesture_choices()
+playerselect = Game()
+playerselect.player_selection()
