5d35fb3f165e5dd51062c62d217be4d1885664c3
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-09-13 08:48:51 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -7,7 +7,7 @@ class Game:
         self.player_two = ''
         self.score_to_win = 2
     def run_game(self):
-        pass
+        pass        
     def welcome_message(self):
         #### Display rules and welcome
         pass
@@ -20,11 +20,13 @@ class Game:
                 print("You have selected single-player! You will play against an AI opponent.")
                 player_one.set_name = input("What is your name?")
                 player_two = AI()
+                
             elif user_input == '2':
                 player_two = Human()
                 print("You have selected multiplayer! please enter your name below. ")
                 player_one.set_name = input("Player 1, what is your real name??")
                 player_two.set_name = input("What is player 2's name?")
+                
             else:
                 user_input = 'playerz'
                 print("Let's make a logical selection here")
@@ -35,16 +37,30 @@ class Game:
     def compare_player_choices(self, player1, player2):
         comparator = ((int(player1) - int(player2)) % 5)
         if comparator == 1 or comparator == 2:
-            print ("Player wins!")
+            print ("Player 1 wins!")
+            player1.round_wins += 1
         elif comparator == 3 or comparator == 4:
-            print ("Computer wins!")
+            player2.round_wins += 1
+            print ("Player 2 wins!")
         else:
-            print ("Player and computer tie!")
+            print ("It's a tie")
         return ""
-        pass
         
-    def best_of_wins(self):
-        pass
+    def best_of_wins(self, scoretowin):
+        if self.player_one.score >= int(scoretowin):
+            pass
+        elif self.player_two.score >= int(scoretowin):
+            pass
+
+    def pick_a_card(self, player):
+        if player.name != "Computer":
+            player.display_gesture_choices()
+            thechosenone = int(input("Pick a your gesture")) - 1
+            return thechosenone
+        else:
+            thechosenone = player.choose_gesture()
+            return thechosenone
+            
     def score_tracker(self, winner):
         winner.round_wins += 1
     def display_winner(self, player1, player2):
